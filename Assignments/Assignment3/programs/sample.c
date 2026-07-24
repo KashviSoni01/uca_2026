ac510014b82d45ef572de3925c80d147635062f8
KashviSoni01 <sonikashvi15@gmail.com>
2026-07-24 11:46:56 +0000

    Added Program for Removing Comments

---
+++ Assignments/Assignment3/programs/sample.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+// This is a comment
+
+int main() {
+
+    printf("Hello // World\n");
+
+    char ch = '/';
+
+    /* This is
+       a multiline
+       comment */
+
+    printf("/* Not a comment */");
+
+    return 0;
+}
